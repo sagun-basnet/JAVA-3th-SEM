4eedaaf1e3fbd53120a55c278bbf1a66efd3060f
sagun basnet <basnetsagun8@gmail.com>
2022-11-09 18:03:38 +0545

    commited

---
+++ MethodOverload.java
@@ -0,0 +1,36 @@
+
+//Example of method overload with construction.. 
+
+public class MethodOverload {
+    MethodOverload(){
+        System.out.println("Construction called");
+    }
+
+    //main method
+    public static void main(String[] args){
+        add(12,34);//called with 2 parameters
+        add(12.5,34.6);//called with 2 parameters
+        add(12,34,4);//called with multiple parameter
+    }
+
+    //for sum
+    public static void add(int x, int y){
+        int z = x+y;
+        System.out.println(z);
+        MethodOverload call = new MethodOverload();
+    }
+
+    //for different datatypes
+    public static void add(double x, double y){
+        double z = x+y;
+        System.out.println(z);
+        MethodOverload call = new MethodOverload();
+    }
+
+
+    public static void add(int x, int y, int w){
+        int z = x+y+w;
+        System.out.println(z);
+        MethodOverload call = new MethodOverload();
+    }
+}
